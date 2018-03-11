b53e8cbf5d6fe08e43b284a8024ff793acf91510
LiorArmon <37212163+LiorArmon@users.noreply.github.com>
2018-03-11 12:10:25 +0200

    Add files via upload

--- students.py
+++ students.py
@@ -1,5 +1,5 @@
-history=dict(Mark=100, Lisa=95, Danny=50, Johnny=20, Peter=80)
-math=dict(Mark=90, Lisa=100, Danny=51, Johnny=15)
+history=dict(Mark=[100,50], Lisa=[95,80], Danny=[50,65], Johnny=[20,15])
+math=dict(Mark=[90,80], Lisa=[100,95], Danny=[51,45], Johnny=[15,16], Peter=[80,70])
 
 def compare_subjects_within_student(history, math):
     """
@@ -8,10 +8,12 @@ def compare_subjects_within_student(history, math):
 
     Choice for the data structure of the function's arguments is up to you.
     """
-    
-for keys in math:
-    if math[keys] > history[keys]:
-        print (keys, 'math', math[keys])
+math_keys=set(math.keys())
+history_keys=set(history.keys())
+shared_students=math_keys & history_keys
+for keys in shared_students:
+    if max(math[keys]) > max(history[keys]):
+        print (keys, 'math', max(math[keys]))
         
     else:
-        print (keys, 'history', history[keys])
+        print (keys, 'history', max(history[keys]))
